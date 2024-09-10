6c9e1e155b24bd81109b027b23d57fd59e59dbe7
202330175 <202330175@PO-24853>
2024-09-10 19:42:47 -0400

    Tout fonctionne étape 5

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -14,8 +14,8 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
     CompteurDeTour tour = new CompteurDeTour();
     AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Personnage("Bob le bricoleur", 70, 15, 15, 15);
-    Personnage Igor = new Personnage("Igor l'empaleur", 100, 25, 5, 30);
+    Personnage Bob = new Personnage("Bob le bricoleur", 15, 15, 70, 15);
+    Personnage Igor = new Personnage("Igor l'empaleur", 25, 5, 100, 30);
     // </editor-fold>
 
     // **************************************************************************
@@ -26,7 +26,7 @@ public class JeuGladiateurs {
     Igor.afficherInfosPersonnage();
     affichage.afficherDebutCombat();
     // </editor-fold>
-
+    
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
