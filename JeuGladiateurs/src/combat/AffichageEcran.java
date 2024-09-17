13ec52804e3a39c9b6c20549da1cac59952d8cca
202330175 <202330175@PO-24853>
2024-09-17 13:07:31 -0400

    Terminé

--- JeuGladiateurs/src/combat/AffichageEcran.java
+++ JeuGladiateurs/src/combat/AffichageEcran.java
@@ -1,47 +1,57 @@
+
 package combat;
 
 import personnages.Personnage;
 
-public class AffichageEcran {
+public class AffichageEcran 
+{
 
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
+    
     // <editor-fold defaultstate="collapsed" desc="Méthodes d'affichage">
-    public void afficherDebutCombat() {
+    public void afficherDebutCombat() 
+    {
         System.out.println("\n");
         System.out.println("<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>");
         System.out.println(">>>>> Que le combat commence ! <<<<<");
         System.out.println("<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>");
     }
     
-    public void afficherSeparateurDeTour() {
+    public void afficherSeparateurDeTour() 
+    {
         System.out.println("\n");
         System.out.println("**************************************");
     }
 
-    public void afficherSeparateurInfosPerso() {
+    public void afficherSeparateurInfosPerso() 
+    {
         System.out.println("\n");
         System.out.println("-----------------");
         System.out.println("INFOS PERSONNAGES");
         System.out.println("-----------------");
     }
 
-    public void afficheVictoire(Personnage Bob, Personnage Igor) {
+    public void afficheVictoire(Personnage Bob, Personnage Igor) 
+    {
         System.out.println("\n");
         
-        if (Bob.getPointDeVie() > 0) {
+        if (Bob.getPointDeVie() > 0) 
+        {
             System.out.println(Bob.getNom() + " gagne le combat !!!");
             System.out.println("\n");
         } 
-        else if (Igor.getPointDeVie() > 0) {
+        else if (Igor.getPointDeVie() > 0) 
+        {
             System.out.println(Igor.getNom() + " gagne le combat !!!");
             System.out.println("\n");
         } 
-        else {
+        else 
+        {
             System.out.println("Aucun des deux personnages n'a gagné. C'est un match nul !");
             System.out.println("\n");
-    }
-}
+        }
+    }   
     // </editor-fold>
 }
