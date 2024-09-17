29eb3c46d59f56249480e47bdddcadd1e3fdc703
202330175 <202330175@PO-24853>
2024-09-17 11:22:20 -0400

    Partie 1, réparée + terminée !

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -8,37 +8,37 @@ public class JeuGladiateurs {
 
     public static void main(String[] args) {
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
-    CompteurDeTour tour = new CompteurDeTour();
-    AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Personnage("Bob le bricoleur", 15, 15, 70, 15);
-    Personnage Igor = new Personnage("Igor l'empaleur", 25, 5, 100, 30);
-    // </editor-fold>
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
+        CompteurDeTour tour = new CompteurDeTour();
+        AffichageEcran affichage = new AffichageEcran();
+        Personnage Bob = new Personnage("Bob le bricoleur", 15, 15, 70, 15);
+        Personnage Igor = new Personnage("Igor l'empaleur", 25, 5, 100, 30);
+        // </editor-fold>
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
-    Bob.afficherInfosPersonnage();
-    Igor.afficherInfosPersonnage();
-    affichage.afficherDebutCombat();
-    // </editor-fold>
-    
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
+        Bob.afficherInfosPersonnage();
+        Igor.afficherInfosPersonnage();
+        affichage.afficherDebutCombat();
+        // </editor-fold>
+        
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
         while (Bob.getPointDeVie() > 0 && Igor.getPointDeVie() > 0) {
             tour.afficheTour();
 
             for (int i = 0; i <= 100; i++) {
-                if (i == Bob.getIniative()) {
+                if (i == Bob.getInitiative()) {
                     Bob.frapperPersonnage(Igor);
                 }
-                if (i == Igor.getIniative()) {
+                if (i == Igor.getInitiative()) {
                     Igor.frapperPersonnage(Bob);
                 }
             }
@@ -54,9 +54,10 @@ public class JeuGladiateurs {
             tour.augmenteTour();
 
             affichage.afficherSeparateurDeTour();
-            
-            affichage.afficheVictoire(Bob, Igor);
         }
-    // </editor-fold><
+        
+        // Appel de `afficheVictoire()` après la fin du combat
+        affichage.afficheVictoire(Bob, Igor);
+    // </editor-fold>
     }
 }
